b19f8318228141fc385626808e0bbba01449ebe1
mlakic <43626884+mlakic@users.noreply.github.com>
2019-03-03 19:23:53 -0800

    Update drama.h

--- drama.h
+++ drama.h
@@ -1,9 +1,9 @@
-// ------------------------------------------------ file name ---------------------------------------------------------
+// ------------------------------------------------ Drama ---------------------------------------------------------
 // Kyle Lotterer, Jonathan Rojas, Marko Lakic CSS 343 C
 // 3/3/2019
 // Date of Last Modification
 // --------------------------------------------------------------------------------------------------------------------
-// Purpose - a brief statement of the program's function
+// Purpose - Child class of DVD for Drama type DVD’s.
 // --------------------------------------------------------------------------------------------------------------------
 // Notes on specifications, special algorithms, and assumptions.
 // --------------------------------------------------------------------------------------------------------------------
@@ -16,9 +16,13 @@ using namespace std;
 class Drama : public DVD
 {
 public:
+	//Constructor
 	Drama();
+	
+	//Deconstructor
 	~Drama();
-
+	
+	//Copy Constructor
 	Drama(const Drama& copy);
 
 private:
