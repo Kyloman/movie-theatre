384f8794cc7e5d3d755e0f32a039bd22dd178239
Kyloman <kyloman@gmail.com>
2019-03-03 19:25:02 -0800

    adding transaction header file

---
+++ transaction.h
@@ -0,0 +1,29 @@
+// ------------------------------------------------ transaction.h ---------------------------------------------------------
+// Kyle Lotterer, Jonathan Rojas, Marko Lakic CSS 343 C
+// 3/3/2019
+// Date of Last Modification
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function
+// --------------------------------------------------------------------------------------------------------------------
+// Notes on specifications, special algorithms, and assumptions.
+// --------------------------------------------------------------------------------------------------------------------
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+#include "media.h"
+#include "customer.h"
+
+class Media
+{
+public:
+	Media();
+	~Media();
+
+	void printData();
+
+private:
+	Media type;
+	Customer owner;
+};
+
+#endif 
