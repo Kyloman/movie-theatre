da469678f256f79b032bf15fd562e976920e3877
mlakic <43626884+mlakic@users.noreply.github.com>
2019-03-03 18:09:37 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// ------------------------------------------------ file name ---------------------------------------------------------
+// Kyle Lotterer, Jonathan Rojas, Marko Lakic CSS 343 C
+// 3/3/2019
+// Date of Last Modification
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - Creates a single shop object which reads all the files from supportingFiles.zip to test and make sure all 
+// functions are working properly. 
+// --------------------------------------------------------------------------------------------------------------------
+// Notes on specifications, special algorithms, and assumptions.
+// --------------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include "shop.h"
+
+using namespace std;
+
+int main()
+{
+	Shop shroudStore;
+	shroudStore.read something();
+	shroudStore.read somethingelse();
+	shroudStore.read lastsomething();
+	
+	return 0;
+}
