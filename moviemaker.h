4c4591abacb7c9cd727104a085d77a20fe51cdd6
mlakic <43626884+mlakic@users.noreply.github.com>
2019-03-03 17:34:23 -0800

    moviemaker.h

---
+++ moviemaker.h
@@ -0,0 +1,39 @@
+// ------------------------------------------------ moviemaker.h ---------------------------------------------------------
+// Kyle Lotterer, Jonathan Rojas, Marko Lakic CSS 343 C
+// 3/3/2019
+// Date of Last Modification
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - Reads in data from text files provided in supportingFiles.zip and creates movies using the included movie 
+// types.
+// --------------------------------------------------------------------------------------------------------------------
+// Notes on specifications, special algorithms, and assumptions.
+// --------------------------------------------------------------------------------------------------------------------
+
+#ifndef MOVIEMAKER_H
+#define MOVIEMAKER_H
+
+#include "movie.h"
+#include "comedy.h"
+#include "drama.h"
+#include "classic.h"
+
+using namespace std;
+
+class
+{
+public:
+    // ------------------------------make------------------------------------
+    // creates a movie, calls helper function after determining movie type
+    // ----------------------------------------------------------------------
+	Movie *make(istream &in);   
+private:
+    // ------------------------helperFunctions-------------------------------
+    // helper functions are called to make specific style of Movie.
+    // ----------------------------------------------------------------------
+	Comedy *makeComedy
+	Drama *makeDrama
+	Classic *makeClassic 
+
+};
+
+#endif 
