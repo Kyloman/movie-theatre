c35e7a86c050ee4d9d7b241d4d9ed225134fae42
Jonny0106 <48191314+Jonny0106@users.noreply.github.com>
2019-03-03 19:55:21 -0800

    Create shop.h

---
+++ shop.h
@@ -0,0 +1,35 @@
+// ------------------------------------------------ shop.h ---------------------------------------------------------
+// Kyle Lotterer, Jonathan Rojas, Marko Lakic CSS 343 C
+// 3/3/2019
+// Date of Last Modification
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - The class in which text files are parsed and processed into internal data. Once processed, 
+// these different types of media will be stored in a Hash Table inside of the class
+// --------------------------------------------------------------------------------------------------------------------
+// Notes on specifications, special algorithms, and assumptions.
+// --------------------------------------------------------------------------------------------------------------------
+
+#ifndef _H
+#define _H
+using namespace std;
+
+class Shop
+{
+public:
+      //constructors
+      Shop();
+      //destructors
+      ~shop();
+      
+      readFile(ifstream& infile)
+      
+      void printInventory();
+      
+Private:
+      club Club;
+      BST comedy;
+      BST drama;
+      BST classic;
+};
+
+#endif 
