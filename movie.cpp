408dffa294cdd2a6a2ca21a82374d2a00d774678
Kyle Lotterer <kyloman@gmail.com>
2019-03-19 04:47:22 -0700

    fixing up movie.cpp

--- movie.cpp
+++ movie.cpp
@@ -7,6 +7,8 @@
 //
 
 #include "movie.h"
+using namespace std;
+
 //Constructor
 Movie::Movie()
 {
@@ -30,20 +32,60 @@ void Movie::setTitle(string name)
 }
 
 //Getters
-void Movie::getDate(string name)
+void Movie::getName()
 {
-    
+	return this.name;
 }
 string Movie::getDirector()
 {
-    return " ";
+    return this.director;
 }
 string Movie::getTitle()
 {
     
-    return " ";
+    return this.title;
 }
 string Movie::getDate()
 {
-    return " ";
+    return this.date;
+}
+
+//Had to guess what the vars were, you magic tricked movie.h out of repo
+string Movie::toString() {
+	return (this.getTitle() + " (" + this.getDate() + ")")
+}
+
+bool Movie::operator==(const Movie& other) {
+	//Check titles
+	string title1 = this.getTitle();
+	string title2 = other.getTitle(); 
+
+	if (title1.length != title2.length) {
+		return false;
+	}
+	
+	for (int x = 0; x < title1.lenth; x++) {
+		if (title1.charAt(x) != title2.charAt(x)) {
+			return false;
+		}
+	}
+
+	//Check dates
+	string date1 = this.getDate();
+	string date2 = other.getDate();
+
+	if (date1.length != date2.length) {
+		return false;
+	}
+
+	for (int x = 0; x < date1.lenth; x++) {
+		if (date1.charAt(x) != date2.charAt(x)) {
+			return false;
+		}
+	}
+
+	//If these are both the same, we assume they are the same!
+	return true;
+}
+
 }
